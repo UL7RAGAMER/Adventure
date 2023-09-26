29b3f80d3b6b325d83bc28330b89a07b11e59ea3
UL7RAGAMER <siddkum@outlook.com>
2023-09-26 20:58:52 +0530

    v15

---
+++ testet.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var tw = get_tree().create_tween()
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+
+		tw.tween_property($".",'position',get_global_mouse_position(),1 ).set_ease(Tween.EASE_OUT).from_current()
+		
+		tw.parallel().tween_property($".",'rotation',deg_to_rad(360),1 ).set_ease(Tween.EASE_OUT).from_current()
+		await tw.finished
+		rotation_degrees = 0
+		print(rotation_degrees)
