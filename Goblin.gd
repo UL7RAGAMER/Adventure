1ceb50a73739cc7d49b615ee932fb8ce120cc46c
UL7RAGAMER <siddkum@outlook.com>
2024-01-05 06:42:07 +0530

    v27

--- Goblin.gd
+++ Goblin.gd
@@ -7,9 +7,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var flip_node : Node2D
 @onready var anitree : AnimationTree = $AnimationTree
 @onready var path : Path2D = $Path2D
-
+var jumped = false
 var ground_in_front : bool = true
-
+signal jp
 @onready var direc = (%Player.global_position.x - $".".global_position.x )
 @export var move_direction : Vector2 = Vector2.RIGHT :  
 	set(new_direction):
@@ -18,7 +18,7 @@ var ground_in_front : bool = true
 			flip_node.scale.x = new_direction.x
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+
 	if not is_on_floor():
 		velocity.y += (gravity) * delta
 	elif (not ground_detector.has_overlapping_bodies() or (is_on_wall())):
@@ -30,16 +30,16 @@ func _process(delta):
 			else:
 				jump()
 	
-	update()
-	update_path(delta)
+	update(delta)
+	
 	move_and_slide()
 	pass
-func update():
+func update(d):
 	var pos = %Player.global_position.x - $".".global_position.x
-	if (pos < 210 and pos > 0) or (pos > -210 and pos < 0)  :
+	if ((pos < 210 and pos > 0) or (pos > -210 and pos < 0) ) and jumped == false:
 		direc = (%Player.global_position - $".".global_position ).normalized()
 		move_direction = direc
-
+		update_path(d)
 	else:
 		velocity.x = 0
 		anitree.set('parameters/Change/transition_request','Idle' )
@@ -56,37 +56,44 @@ func update_path(d):
 	var pos = %Player.global_position.x - $".".global_position.x
 	var pos2 = path.curve.get_point_position(2)
 	var pos1 = path.curve.get_point_position(1)
-	if (pos < 210 and pos > 0) or (pos > -210 and pos < 0)  :
-		var plp = (%Player.global_position - $".".global_position)
-		
-		plp.y = 0
-		pos1.x = pos2.x/2
-		path.curve.set_point_position(2,plp)
-		if plp.x < 0: 
-			path.curve.set_point_in(1,Vector2(58,0))
-			path.curve.set_point_out(1,Vector2(-58,0))
-		else:
-			path.curve.set_point_in(1,Vector2(-58,0))
-			path.curve.set_point_out(1,Vector2(58,0))
-		path.curve.set_point_position(1,pos1)
-		if r <= 0.428:
-			attack_j(d)
-		elif r == 1:
-			reset(plp)
-			
+	var pos0 = path.curve.get_point_position(0)
+	var plp = (%Player.global_position - $".".global_position)
+	pos1.x = pos2.x/2
+	$Path2D/Marker2D.position = pos0	
+	$Path2D/Marker2D2.position = pos1
+	$Path2D/Marker2D3.position = pos2
+	path.curve.set_point_position(1,pos1)
+	if jumped == false:
+		var p = path.curve.get_point_count()
+		$Label.set_text(str(p))
+		path.curve.remove_point(3)
+		path.curve.remove_point(4)
+
+		if (pos < 210 and pos > 0) or (pos > -210 and pos < 0)  :
+			path.curve.set_point_position(2,plp)
+			if plp.x < 0: 
+				path.curve.set_point_in(1,Vector2(58,0))
+				path.curve.set_point_out(1,Vector2(-58,0))
+			else:
+				path.curve.set_point_in(1,Vector2(-58,0))
+				path.curve.set_point_out(1,Vector2(58,0))
+
+			if r <= 0.428 and jumped == false:
 				
-		else:
-			attack_f(d)
-	pass
+				attack_j(d)
+			elif r == 1:
+				reset(plp)
+				
+					
+			elif r >= 0.428 and jumped == false:
+				attack_f(d)
+		pass
 func attack_j(d):
 	await  get_tree().create_timer(0.2).timeout
 	var r = $Path2D/PathFollow2D.get_progress_ratio()
-	$Label.set_text(str(r))
+
 	if r <= 0.428:
 		$Path2D/PathFollow2D.set_progress_ratio(r + ft(0.45))
-	
-		
-	#Change the animation to fix
 	anitree.set('parameters/Change/transition_request','Atk_jump')
 	if move_direction.x < 0:
 		anitree.set('parameters/Atk_j/transition_request','Atk_flipped')
@@ -96,7 +103,6 @@ func attack_j(d):
 func attack_f(d):
 	var r = $Path2D/PathFollow2D.get_progress_ratio()
 	if r != 1:
-		$Label.set_text(str(r))
 		$Path2D/PathFollow2D.set_progress_ratio(r + ft(0.3))
 		anitree.set('parameters/Change/transition_request','Atk_fall')
 		if move_direction.x < 0:
@@ -109,16 +115,25 @@ func attack_f(d):
 		pass
 		
 func reset(plp):
-	var pos0 = path.curve.get_point_position(0)
-	var pos2 = path.curve.get_point_position(2)
-	path.curve.remove_point(0)
-	path.curve.remove_point(1)
-	path.curve.remove_point(2)
-	$Path2D/PathFollow2D.set_progress_ratio(0)
-	path.curve.add_point(Vector2.ZERO,Vector2.ZERO,Vector2.ZERO,0)
-	path.curve.add_point(Vector2(0,-65),Vector2.ZERO,Vector2.ZERO,1)
-	path.curve.add_point(plp,Vector2.ZERO,Vector2.ZERO,2)
+	if jumped == false :
+		$Label.set_text('Reset')
+		var pos0 = path.curve.get_point_position(0)
+		var pos2 = path.curve.get_point_position(2)
+		path.curve.clear_points()
+		$Path2D/PathFollow2D.set_progress(0)
+		var p = $Path2D/Marker2D3.global_position
+		$".".global_position = Vector2(p.x,p.y - 10)
+		path.curve.add_point(Vector2.ZERO,Vector2.ZERO,Vector2.ZERO,0)
+		path.curve.add_point(Vector2(0,-65),Vector2.ZERO,Vector2.ZERO,1)
+		path.curve.add_point(plp,Vector2.ZERO,Vector2.ZERO,2)
+		jumped = true
+		jp.emit()
 
-	pass
 func ft(x):
 	return 1/(200 * x)
+
+
+func _on_jp():
+	await get_tree().create_timer(3).timeout
+	jumped = false
+	pass # Replace with function body.
